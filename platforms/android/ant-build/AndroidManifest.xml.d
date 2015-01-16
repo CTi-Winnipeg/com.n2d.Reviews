32839605b96b34d3b8eb1a4d8acfd1108bf09489
Richard Bennett <richard.bennett.cti@gmail.com>
2015-01-16 13:46:22 -0600

    Added Android Platform

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/richbennett/Documents/_dev/phonegap/com.n2d.Reviews/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/richbennett/Documents/_dev/phonegap/com.n2d.Reviews/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/richbennett/Documents/_dev/phonegap/com.n2d.Reviews/platforms/android/ant-build/classes.dex : \
+/Users/richbennett/Documents/_dev/phonegap/com.n2d.Reviews/platforms/android/ant-build/classes/com/n2d/reviews/BuildConfig.class \
+/Users/richbennett/Documents/_dev/phonegap/com.n2d.Reviews/platforms/android/ant-build/classes/com/n2d/reviews/CordovaApp.class \
+/Users/richbennett/Documents/_dev/phonegap/com.n2d.Reviews/platforms/android/ant-build/classes/com/n2d/reviews/R$attr.class \
+/Users/richbennett/Documents/_dev/phonegap/com.n2d.Reviews/platforms/android/ant-build/classes/com/n2d/reviews/R$drawable.class \
+/Users/richbennett/Documents/_dev/phonegap/com.n2d.Reviews/platforms/android/ant-build/classes/com/n2d/reviews/R$string.class \
+/Users/richbennett/Documents/_dev/phonegap/com.n2d.Reviews/platforms/android/ant-build/classes/com/n2d/reviews/R$xml.class \
+/Users/richbennett/Documents/_dev/phonegap/com.n2d.Reviews/platforms/android/ant-build/classes/com/n2d/reviews/R.class \
+/Users/richbennett/Documents/_dev/phonegap/com.n2d.Reviews/platforms/android/ant-build/dexedLibs/classes-1e2ac6027aae4c97ac41b72a1c492fec.jar \
